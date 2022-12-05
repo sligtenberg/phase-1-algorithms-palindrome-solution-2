fc497fd9c474695732b923c8ef7df165050518c7
stephen ligtenberg <bountyhunter24@gmail.com>
2022-12-05 17:48:02 -0500

    manual commit

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    if (word[i] != word[word.length - 1 - i]) return false;
+  }
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  iterate through the word, comparing the first letter to the last, and moving towards the middle
 */
 
 /*
